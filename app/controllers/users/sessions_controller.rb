c2abb30dd9dc9d6c82632f195d0f6fb4f9446319
ycbencheng <ycbencheng@gmail.com>
2021-12-08 11:06:06 -0700

    Create authentication controllers, and users

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,36 @@
+class Users::SessionsController < Devise::SessionsController
+  respond_to :json
+
+  def create
+    @user = User.find_by(email: params['session']['email'])
+
+    if @user.valid_password?(params['session']['password'])
+      sign_in(:user, @user)
+      respond_with(@user)
+    end
+  end
+
+  private
+
+  def respond_with(resource, _opts = {})
+    token = resource.allowlisted_jwts.first.jti
+
+    render json: { message: 'You are logged in.',
+                   token: token,
+                   status: :success }
+  end
+
+  def respond_to_on_destroy
+    return log_out_success if !current_user
+
+    log_out_failure
+  end
+
+  def log_out_success
+    render json: { message: 'You are logged out.', status: :success }
+  end
+
+  def log_out_failure
+    render json: { message: 'Hmm nothing happened.', status: :failed }
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 2021_12_07_174719) do
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "deleted_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
